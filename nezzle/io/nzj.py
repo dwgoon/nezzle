a3a75f4726dfc1994344796bc2a23bfd758fb0dd
Daewon Lee <daewon4you@gmail.com>
2022-04-13 03:56:38 +0900

    Fix dumping json

--- nezzle/io/nzj.py
+++ nezzle/io/nzj.py
@@ -2,8 +2,24 @@ import json
 import codecs
 from collections import defaultdict
 
+import numpy as np
+
 from nezzle.graphics import Network
 
+
+class NpEncoder(json.JSONEncoder):
+    """
+    [Reference] https://stackoverflow.com/a/57915246
+    """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+# end of class
 
 def read_metadata_from_nzj(fpath):
     metadata = {}
@@ -36,5 +52,5 @@ def read_nzj(fpath, edge_map):
 
 def write_nzj(net, fpath):
     with codecs.open(fpath, "w", encoding="utf-8") as fout:
-        fout.write(json.dumps(net.to_dict()))
+        fout.write(json.dumps(net.to_dict(), cls=NpEncoder))
 # end of def
